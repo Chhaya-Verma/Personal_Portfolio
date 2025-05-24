61a98e8653f49d18c47edb528531f1b7da8c9fd6
Chhaya-Verma <chhayav300@gmail.com>
2025-05-25 00:13:30 +0530

    Added Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+// pages/Home.tsx
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-900 text-white text-center">
+      <h1 className="text-4xl font-bold">Welcome to My Portfolio</h1>
+    </div>
+  );
+};
+
+export default Home;
